29a4a87beed75a19f9af7d301c8aa874f12d79b4
sondercode <sondercode@gmail.com>
2015-01-24 15:25:25 +0000

    Initial Scene set up and basic scripts

---
+++ GameJam2015Unity/Assets/Scripts/Main Menu/ExitGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitGame : MonoBehaviour {
+
+	public void ExitTheGame()
+	{
+		#if UNITY_EDITOR
+		Debug.Log ("Quit");
+		#endif
+		Application.Quit();
+	}
+
+}
---
+++ GameJam2015Unity/Assets/Scripts/SceneChange/ChangeSceneTo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSceneTo : MonoBehaviour {
+
+	public void SwapSceneName(string Scenename)
+	{
+		Application.LoadLevel(Scenename);
+	}
+
+	public void SwapSceneNumber(int Scenenumber)
+	{
+		Application.LoadLevel(Scenenumber);
+	}
+}
